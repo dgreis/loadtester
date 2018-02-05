e13d814e225db79781af52278426db50b6c7640f
David Greis <2243125+dgreis@users.noreply.github.com>
2018-02-05 18:38:10 +0800

    add product page with stylings (template lifted from online resource)

---
+++ web/javascript/conversion.js
@@ -0,0 +1,15 @@
+/**
+ * Created by davidgreis on 2/5/18.
+ */
+
+$(document).ready(function() {
+
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+
+});
